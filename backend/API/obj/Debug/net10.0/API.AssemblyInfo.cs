0f266cb4443f2e338f30d6ca011f18a0fd099220
karim_tarek_/ <karimxgsg@gmail.com>
2026-07-12 14:24:49 +0300

    fix bugs

--- backend/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ backend/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4b4cbc398733e04fc4cf4d8070ee8137be7b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
